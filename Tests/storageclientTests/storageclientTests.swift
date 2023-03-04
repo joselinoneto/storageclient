e4f8285a75fad21d63239361d0f46c348d7b0db0
José Neto <jneto@blissapplications.com>
2023-03-04 18:55:38 +0000

    fix search

--- Tests/storageclientTests/storageclientTests.swift
+++ Tests/storageclientTests/storageclientTests.swift
@@ -35,7 +35,7 @@ final class storageclientTests: XCTestCase {
     }
 
     func testSQLBaseStorage() throws {
-        let storage = ApodStorageController(inMemory: false)
+        let storage = ApodStorageController(inMemory: true)
         let explanation: String = "Spiral galaxy NGC 3169 looks to be unraveling like a ball of cosmic yarn. It lies some 70 million light-years away, south of bright star Regulus toward the faint constellation Sextans. Wound up spiral arms are pulled out into sweeping tidal tails as NGC 3169 (left) and neighboring NGC 3166 interact gravitationally. Eventually the galaxies will merge into one, a common fate even for bright galaxies in the local universe. Drawn out stellar arcs and plumes are clear indications of the ongoing gravitational interactions across the deep and colorful galaxy group photo. The telescopic frame spans about 20 arc minutes or about 400,000 light-years at the group's estimated distance, and includes smaller, bluish NGC 3165 at the right. NGC 3169 is also known to shine across the spectrum from radio to X-rays, harboring an active galactic nucleus that is the site of a supermassive black hole."
 
         let item = ApodStorage()
@@ -84,19 +84,36 @@ final class storageclientTests: XCTestCase {
     }
 
     func testCrudFilter() async throws {
-        let controller = ApodStorageController(inMemory: false)
+        let controller = ApodStorageController(inMemory: true)
         let date = Date()
         let mock = ApodStorage()
         let id = UUID()
         mock.id = id
         mock.title = "MockTitle"
         mock.postedDate = date
-        try await controller.asyncSaveItem(mock)
+        mock.date = "2023-02-01"
 
-        let items = try controller.searchApods(startMonth: date, endMonth: date)
+        let mock1 = ApodStorage()
+        mock1.id = UUID()
+        mock1.title = "MockTitle"
+        mock1.postedDate = date
+        mock1.date = "2023-02-02"
+
+        let mock2 = ApodStorage()
+        mock2.id = UUID()
+        mock2.title = "MockTitle"
+        mock2.postedDate = date
+        mock2.date = "2023-03-01"
+
+        try await controller.asyncSaveItem(mock)
+        try await controller.asyncSaveItem(mock1)
+        try await controller.asyncSaveItem(mock2)
+
+        let items = try controller.searchApods(startMonth: "2023-02-01", endMonth: "2023-02-28")
 
         XCTAssertNotNil(items)
         XCTAssertEqual(items?.first?.id, mock.id)
+        XCTAssertEqual(items?.count, 2)
     }
 
     func testCrudAsync() async throws {
