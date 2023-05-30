4bd906960bc7a6c323d0e62099daee068199d232
José Neto <jneto@blissapplications.com>
2023-05-30 23:37:09 +0100

    sql batch

--- Tests/storageclientTests/storageclientTests.swift
+++ Tests/storageclientTests/storageclientTests.swift
@@ -34,7 +34,7 @@ final class storageclientTests: XCTestCase {
         XCTAssertEqual(array?.first?.id, item.id)
     }
 
-    func testSQLBaseStorage() throws {
+    func testSQLBaseStorage() async throws {
         let storage = ApodStorageController(inMemory: true)
         let explanation: String = "Spiral galaxy NGC 3169 looks to be unraveling like a ball of cosmic yarn. It lies some 70 million light-years away, south of bright star Regulus toward the faint constellation Sextans. Wound up spiral arms are pulled out into sweeping tidal tails as NGC 3169 (left) and neighboring NGC 3166 interact gravitationally. Eventually the galaxies will merge into one, a common fate even for bright galaxies in the local universe. Drawn out stellar arcs and plumes are clear indications of the ongoing gravitational interactions across the deep and colorful galaxy group photo. The telescopic frame spans about 20 arc minutes or about 400,000 light-years at the group's estimated distance, and includes smaller, bluish NGC 3165 at the right. NGC 3169 is also known to shine across the spectrum from radio to X-rays, harboring an active galactic nucleus that is the site of a supermassive black hole."
 
@@ -56,7 +56,7 @@ final class storageclientTests: XCTestCase {
         item2.explanation = explanation
         item2.postedDate = Date()
 
-        try storage.saveItemsSql([item, item1, item2])
+        try await storage.saveItemsSql([item, item1, item2])
 
         let items = try storage.getAllItems()
 
